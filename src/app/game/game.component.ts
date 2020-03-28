08f7fa79c8318ed712d1d93bb59d2d0dc99304a9
Salmin Skenderovic <salmin.skenderovic@consid.se>
2020-03-28 12:48:19 -0400

    moved logic into service

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import GameService from './game.service';
+import GoalTile from './GoalTile';
+import GameTile from './GameTile';
 
 @Component({
   selector: 'app-game',
@@ -6,117 +9,42 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent {
-  gameTiles: GameTile[] = []
-  goalTiles: GoalTile[] = []
-  bottomGoalTiles: GoalTile[] = []
+  gameTiles: GameTile[] = [];
+  goalTiles: GoalTile[] = [];
+  bottomGoalTiles: GoalTile[] = [];
   gameWon: boolean;
-
-  Arr = Array;
   @Input() level: number;
 
+  constructor(private gameService: GameService) {}
+
   ngOnChanges(): void {
-    this.setup(this.level)
+    this.setup(this.level);
   }
 
   setup(level: number) {
-    this.gameTiles = []
-    this.goalTiles = []
-    this.bottomGoalTiles = []
-
-    let goalTile = new GoalTile()
-
-    for (let i = 0; i < level; i++) {
-      this.bottomGoalTiles.push(new GoalTile())
-    }
-
-    for (let i = 0; i < level * level; i++) {
-      const gameTile = new GameTile(i);
-      gameTile.add(goalTile)
-      gameTile.add(this.bottomGoalTiles[i % level])
-      this.gameTiles.push(gameTile)
-      goalTile.add(gameTile)
-
-      //update goal tile with a new one
-      if (i !== 0 && (i + 1) % level === 0) {
-        this.goalTiles.push(goalTile)
-        goalTile = new GoalTile()
-      }
-
-      this.bottomGoalTiles[i % level].add(gameTile)
-    }
-
-    // set the new random values
-    this.goalTiles.forEach(g => g.setValue())
-    this.bottomGoalTiles.forEach(g => g.setValue())
-
-    // reset active
-    this.gameTiles.forEach(gameTile => gameTile.active = false)
+    const [gameTiles, goalTiles, bottomGoalTiles] = this.gameService.setup(
+      level
+    );
+    this.gameTiles = gameTiles;
+    this.goalTiles = goalTiles;
+    this.bottomGoalTiles = bottomGoalTiles;
   }
 
   handleClick(tile: GameTile) {
     tile.toggle();
-    this.checkGame()
+    this.checkGame();
   }
 
   checkGame() {
-    const won = this.goalTiles.every(g => g.valid)
-      && this.bottomGoalTiles.every(g => g.valid)
+    const won =
+      this.goalTiles.every(g => g.valid) &&
+      this.bottomGoalTiles.every(g => g.valid);
 
     this.gameWon = won;
   }
 
   newGame() {
-    this.gameWon = false
-    this.setup(this.level)
-  }
-}
-
-class GameTile {
-  index: number;
-  active: boolean = false;
-  goalTileRefs: GoalTile[] = []
-
-  constructor(index: number) {
-    this.index = index
-    this.active = Math.random() > 0.5
-  }
-
-  add(goalTileRefs: GoalTile) {
-    this.goalTileRefs.push(goalTileRefs)
-  }
-
-  toggle() {
-    this.active = !this.active;
-    this.goalTileRefs.forEach(goalTile => goalTile.validate())
-  }
-}
-
-class GoalTile {
-  value: number;
-  valid: boolean = false;
-  gameTiles: GameTile[] = [];
-
-  add(gameTile: GameTile) {
-    this.gameTiles.push(gameTile)
-  }
-
-  setValue() {
-    const newValue = this.getValue()
-    this.value = parseInt(newValue, 2)
-    if (this.value === 0) {
-      this.valid = true
-    }
-  }
-
-  private getValue() {
-    return this.gameTiles.reduce((acc: string, curr) => {
-      acc += curr.active ? "1" : "0"
-      return acc
-    }, "")
-  }
-
-  validate() {
-    const bin = this.getValue()
-    this.valid = parseInt(bin, 2) === this.value
+    this.gameWon = false;
+    this.setup(this.level);
   }
 }
---
+++ src/app/game/game.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import GameTile from './GameTile';
+import GoalTile from './GoalTile';
+
+@Injectable({
+  providedIn: 'root'
+})
+export default class GameService {
+  private gameTiles = [];
+  private goalTiles = [];
+  private bottomGoalTiles = [];
+
+  public setup(level: number) {
+    // Empty state
+    this.gameTiles = [];
+    this.goalTiles = [];
+    this.bottomGoalTiles = [];
+
+    this.generateTiles(level);
+
+    this.setRandomValues();
+
+    this.resetActive();
+
+    return [this.gameTiles, this.goalTiles, this.bottomGoalTiles];
+  }
+
+  private resetActive() {
+    this.gameTiles.forEach(gameTile => (gameTile.active = false));
+  }
+
+  private setRandomValues() {
+    this.goalTiles.forEach(g => g.setValue());
+    this.bottomGoalTiles.forEach(g => g.setValue());
+  }
+
+  private generateTiles(level: number) {
+    let goalTile = new GoalTile();
+    for (let i = 0; i < level; i++) {
+      this.bottomGoalTiles.push(new GoalTile());
+    }
+    for (let i = 0; i < level * level; i++) {
+      const gameTile = new GameTile(i);
+      gameTile.add(goalTile);
+      gameTile.add(this.bottomGoalTiles[i % level]);
+      this.gameTiles.push(gameTile);
+      goalTile.add(gameTile);
+      //update goal tile with a new one
+      if (i !== 0 && (i + 1) % level === 0) {
+        this.goalTiles.push(goalTile);
+        goalTile = new GoalTile();
+      }
+      this.bottomGoalTiles[i % level].add(gameTile);
+    }
+  }
+}
