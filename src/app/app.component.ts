6d612775e68b16061b7cc447726d6722835a8a2a
Salmin Skenderovic <salmin89@hotmail.com>
2020-03-29 21:52:37 -0400

    moved level selector to its own file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  level = 4;
+  public level = 4;
 
-  onChange(e) {
-    this.level = e.target.value
+  public onLevelChange(level: number) {
+    this.level = level
   }
 }
